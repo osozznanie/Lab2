a63205635b3b71eec9e81e02cd771f776877ee96
osozznanie <120659363+osozznanie@users.noreply.github.com>
2023-02-28 21:58:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using oopLab1;
+
+class Application
+{
+    static int x1, y1, x2, y2, x3, y3, x4, y4;
+
+    public static void Main(String[] args)
+    {
+
+        while (true)
+        {
+            menu();
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    consoleInputCalculationArea();
+                    break;
+                case 2:
+                    consoleInputCalculationPerimeter();
+                    break;
+                case 3:
+                    array();
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("You can not use " + num);
+                    break;
+
+            }
+
+
+        }
+    }
+
+    public static void menu()
+    {
+        Console.WriteLine("Press 1 - to calculate the area of ​​the rhombus");
+        Console.WriteLine("Press 2 - to calculate the perimeter of the rhombus");
+        Console.WriteLine("Press 3 - to create array");
+        Console.WriteLine("Press 0 - to exit the program");
+
+    }
+
+    public static void consoleInputCalculationArea()
+    {
+        Console.WriteLine("Enter x1");
+        x1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y1");
+        y1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x2");
+        x2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y2");
+        y2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x3");
+        x3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y3");
+        y3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x4");
+        x4 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y4");
+        y4 = Convert.ToInt32(Console.ReadLine());
+
+        Rhombuses rhombuses = new Rhombuses(x1, y1, x2, y2, x3, y3, x4, y4);
+        Console.WriteLine("Rhombus square = " + rhombuses.squareRhombuses());
+    }
+
+    public static void consoleInputCalculationPerimeter()
+    {
+        Console.WriteLine("Enter x1");
+        x1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y1");
+        y1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x2");
+        x2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y2");
+        y2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x3");
+        x3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y3");
+        y3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x4");
+        x4 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y4");
+        y4 = Convert.ToInt32(Console.ReadLine());
+
+        Rhombuses rhombuses2 = new Rhombuses(x1, y1, x2, y2, x3, y3, x4, y4);
+        Console.WriteLine("Perimeter = " + rhombuses2.perimeterRhombuses());
+    }
+
+    public static void array()
+    {
+        Console.WriteLine("Enter x1");
+        x1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y1");
+        y1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x2");
+        x2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y2");
+        y2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x3");
+        x3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y3");
+        y3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter x4");
+        x4 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter y4");
+        y4 = Convert.ToInt32(Console.ReadLine());
+
+        Rhombuses[] rhombArray = new Rhombuses[17];
+
+        for (int i = 0; i < rhombArray.Length / 2; i++)
+        {
+            rhombArray[i] = new Rhombuses(x1, y1, x2, y2, x3, y3, x4, y4);
+            rhombArray[rhombArray.Length - i - 1] = new Rhombuses(
+                rhombArray[i].getX4(), rhombArray[i].getY4(),
+                rhombArray[i].getX3(), rhombArray[i].getY3(),
+                rhombArray[i].getX2(), rhombArray[i].getY2(),
+                rhombArray[i].getX1(), rhombArray[i].getY1()
+            );
+        }
+
+        for (int i = 0; i < rhombArray.Length; i++)
+        {
+            Console.WriteLine(rhombArray[i]);
+        }
+
+
+    }
+
+  
+}
+
+
+
---
+++ Rhombuses.cs
@@ -0,0 +1,109 @@
+﻿using System;
+namespace oopLab1
+{
+	public class Rhombuses
+	{
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
+        private int x3;
+        private int y3;
+        private int x4;
+        private int y4;
+        public Rhombuses(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+            this.x4 = x4;
+            this.y4 = y4;
+        }        
+
+        public int getX1()
+        {
+            return this.x1;
+        }
+        public void setX1(int x1)
+        {
+            this.x1 = x1;
+        }
+        public int getY1()
+        {
+            return this.y1;
+        }
+        public void setY1(int y1)
+        {
+            this.y1 = y1;
+        }
+        public int getX2()
+        {
+            return this.x2;
+        }
+        public void setX2(int x2)
+        {
+            this.x2 = x2;
+        }
+        public int getY2()
+        {
+            return this.y2;
+        }
+        public void setY2(int y2)
+        {
+            this.y2 = y2;
+        }
+        public int getX3()
+        {
+            return this.x3;
+        }
+        public void setX3(int x3)
+        {
+            this.x3 = x3;
+        }
+        public int getY3()
+        {
+            return this.y3;
+        }
+        public void setY3(int y3)
+        {
+            this.y3 = y3;
+        }
+        public int getX4()
+        {
+            return this.x4;
+        }
+        public void setX4(int x4)
+        {
+            this.x4 = x4;
+        }
+        public int getY4()
+        {
+            return this.y4;
+        }
+        public void setY4(int y4)
+        {
+            this.y4 = y4;
+        }
+        public double squareRhombuses()
+        {
+            var d1 = Math.Sqrt(Math.Pow(this.x2 - this.x1, 2) + Math.Pow(this.y2 - this.y1, 2));
+            var d2 = Math.Sqrt(Math.Pow(this.x4 - this.x3, 2) + Math.Pow(this.y4 - this.y3, 2));
+            return Math.Ceiling(d1 * d2 / 2);
+        }
+        public double perimeterRhombuses()
+        {
+            var d1 = Math.Sqrt(Math.Pow(this.x2 - this.x1, 2) + Math.Pow(this.y2 - this.y1, 2));
+            return Math.Ceiling(d1 * 4);
+        }
+
+        public override string ToString()
+        {
+            return $"Rhombus with vertices ({x1},{y1}), ({x2},{y2}), ({x3},{y3}), ({x4},{y4})";
+        }
+
+    }
+}
+
